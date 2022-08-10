8ef9c923c5665be6c66d09adb7faed161ef3c27b
Johan Persson <johan162@gmail.com>
2022-08-10 14:43:47 +0200

    Added Jenkins Job DSL groovy script to create builders

---
+++ scripts/jenkins_dsl.groovy
@@ -0,0 +1,77 @@
+job('hpdf_table_ub18') {
+  authenticationToken('hpdf_table')
+  scm {
+    git {
+      remote {
+        url('https://github.com/johan162/hpdf_table.git')
+        branch('*/master')
+      }
+    }
+  }
+  properties {
+    githubProjectUrl('https://github.com/johan162/hpdf_table')
+  }  
+  label('ubuntu18')
+  logRotator {
+    numToKeep(3)
+  }
+  wrappers {
+    timestamps()
+    preBuildCleanup()
+  }  
+  steps {
+    shell("./scripts/bootstrap.sh && make -j4")
+  }
+}
+
+job('hpdf_table_ub20') {
+  authenticationToken('hpdf_table')
+  scm {
+    git {
+      remote {
+        url('https://github.com/johan162/hpdf_table.git')
+        branch('*/master')
+      }
+    }
+  }
+  properties {
+    githubProjectUrl('https://github.com/johan162/hpdf_table')
+  }  
+  label('ubuntu20')
+  logRotator {
+    numToKeep(3)
+  }
+  wrappers {
+    timestamps()
+    preBuildCleanup()
+  }  
+  steps {
+    shell("./scripts/bootstrap.sh && make -j4")
+  }
+}
+
+job('hpdf_table_ub22') {
+  authenticationToken('hpdf_table')
+  scm {
+    git {
+      remote {
+        url('https://github.com/johan162/hpdf_table.git')
+        branch('*/master')
+      }
+    }
+  }
+  properties {
+    githubProjectUrl('https://github.com/johan162/hpdf_table')
+  }  
+  label('ubuntu22')
+  logRotator {
+    numToKeep(3)
+  }
+  wrappers {
+    timestamps()
+    preBuildCleanup()
+  }  
+  steps {
+    shell("./scripts/bootstrap.sh && make -j4")
+  }
+}
