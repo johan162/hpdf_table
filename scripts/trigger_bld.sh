adb5196a5b5f26f44d6aeae2d8f8e27cf4d78128
Johan Persson <johan162@gmail.com>
2022-08-23 16:52:44 +0200

    Added trigger script for local Jenkins

--- scripts/trigger_bld.sh
+++ scripts/trigger_bld.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-TOKEN=kallekalle
+# Notes: Assume the existence of JENKINS_HOST and JENKINS_TOKEN environment variables
+
+if [[ -z ${JENKINS_HOST} ]]; then
+	echo "Error. JENKINS_HOST not set."
+	exit 1
+fi
+
+if [[ -z ${JENKINS_TOKEN} ]]; then
+        echo "Error. JENKINS_TOKEN not set."
+        exit 1
+fi
+
 nodes=("hpdf_table_ub18" "hpdf_table_ub20" "hpdf_table_ub22")
 for n in ${nodes[@]}; do
-	curl  "http://192.168.64.12:8080/buildByToken/build?job=${n}&token=${TOKEN}"
+	curl  "http://${JENKINS_HOST}:8080/buildByToken/build?job=${n}&token=${JENKINS_TOKEN}"
 	echo $n
 done
 echo "Done."
